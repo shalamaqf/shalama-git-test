a2516ccee3a750cced3298275dc1b06c747d3563
Shalama Fadilla <shalamafadillaa@gmail.com>
2025-04-10 16:08:01 +0700

    menambahkan variable dan print function

--- Test.java
+++ Test.java
@@ -1,5 +1,8 @@
 public class Test{
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        String name = "Lala";
+        System.out.println("Hello " + name);
     }
 }
